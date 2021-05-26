eed96f9e706e38fb117dfeb9f2c45a6adfc0066f
Laufenberg <jared_laufenberg@fws.gov>
2021-05-26 14:20:24 -0800

    Fix internal assignment operators

--- R/summary.fn.R
+++ R/summary.fn.R
@@ -6,7 +6,7 @@
 #' @export
 #'
 Mode <- function(x) {
-  d <- density(x,na.rm=TRUE)
+  d = density(x,na.rm=TRUE)
   d$x[which.max(d$y)]
 }
 
@@ -18,23 +18,23 @@ Mode <- function(x) {
 #' @export
 #'
 summary.fn	<- function(x=NULL){
-    cn <- c("mean","sd","mode","0%","2.5%","5%","25%","50%","75%","95%","97.5%","100%")
+    cn = c("mean","sd","mode","0%","2.5%","5%","25%","50%","75%","95%","97.5%","100%")
     if(!is.null(x)){
         if(any(is.na(x))){
             cat("\n\nWarning: x contains missing values\n\n")
         } #if
         if(is.matrix(x)){
-            y <- array(0L,c(dim(x)[2],10))
+            y = array(0L,c(dim(x)[2],10))
             for(i in 1:dim(x)[2]){
-                y[i,] <- c(mean(x[,i],na.rm=TRUE), sd(x[,i],na.rm=TRUE), Mode(x[,i]),
+                y[i,] = c(mean(x[,i],na.rm=TRUE), sd(x[,i],na.rm=TRUE), Mode(x[,i]),
                            quantile(x[,i], probs = c(0,0.025,0.05,0.25,0.50,0.75,0.95,0.975,1), na.rm=TRUE))
-                dimnames(y)[[2]] <- cn
-                dimnames(y)[[1]] <- dimnames(x)[[2]]
+                dimnames(y)[[2]] = cn
+                dimnames(y)[[1]] = dimnames(x)[[2]]
             } #i
         }else{
-            y <- c(mean(x,na.rm=TRUE), sd(x,na.rm=TRUE), Mode(x),
+            y = c(mean(x,na.rm=TRUE), sd(x,na.rm=TRUE), Mode(x),
                    quantile(x, probs = c(0,0.025,0.05,0.25,0.50,0.75,0.95,0.975,1), na.rm=TRUE))
-            names(y) <- cn
+            names(y) = cn
         } #ifelse
         return(y)
     }
